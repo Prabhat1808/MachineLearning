31ee4178eae7d7fea781e1b8c3a9540a8876469c
Prabhat1808 <prabhatkanaujia08@gmail.com>
2018-09-05 10:11:36 +0530

    Calculated basic probabilities

---
+++ NaiveBayes/naive.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+import sys
+import math
+import re
+
+TRAIN_FILE = "../../col341_a2_data/amazon_train.csv"
+TEST_FILE = "../../col341_a2_data/amazon_test_public.csv"
+RE1 = "\w+"
+
+def load_data(file_path):
+	dat = pd.read_csv(file_path,header=None,index_col=None)
+	return {"reviews":dat[1].tolist(),"labels":pd.to_numeric(dat[0],downcast='integer').tolist()}
+
+def get_tokens(review):
+	try:
+		tokens = [x for x in review.split(' ')]
+	except:
+		tokens = []
+	return tokens
+
+def create_vocabulary(data):
+	vocab = set()
+	for review in data:
+		words = get_tokens(review)
+		for word in words:
+			vocab.add(word)
+	return list(vocab)
+
+def invert_vocab(vocab):
+	return {vocab[i]:i for i in range(len(vocab))}
+
+def train_model(data,vocab,inv_vocab):
+	reviews = data["reviews"]
+	labels = data["labels"]
+	cc = np.zeros(len(set(labels)))
+	wc = np.zeros((len(set(labels)),len(vocab)))
+	for i in range(len(labels)):
+		tmp = np.zeros(len(vocab))
+		rev = reviews[i]
+		lab = labels[i]
+		cc[lab-1] += 1
+		words = get_tokens(rev)
+		for word in words:
+			tmp[inv_vocab[word]] = 1
+		wc[lab-1] += tmp
+	return {"cc":cc,"wc":wc}
+
+def predict(review,model,vocab,inv_vocab):
+	cc = model["cc"]
+	tot = np.sum(cc)
+	wc = model["wc"]
+	prob = np.zeros(len(cc))
+	words = get_tokens(review)
+	for label in range(len(cc)):
+		tmp = math.log(cc[label]/tot)
+		den = cc[label] + len(vocab) + 1
+		for word in words:
+			try:
+				occurence = cc[label][inv_vocab[word]]
+			except:
+				occurence = 0
+			tmp += math.log((occurence+1)/den)
+		prob[label] = tmp
+	return np.argmax(prob)+1
+
+train_data = load_data(TRAIN_FILE)
+vocab = create_vocabulary(train_data["reviews"])
+inv_vocab = invert_vocab(vocab)
+model = train_model(train_data,vocab,inv_vocab)
+test_data = load_data(TEST_FILE)
+predictions = [int(predict(rev,model,vocab,inv_vocab)) for rev in test_data["reviews"]]
