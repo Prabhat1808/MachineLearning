bd4966f586b922305c911935603c8ae3a9519005
Prabhat1808 <prabhatkanaujia08@gmail.com>
2018-09-07 01:50:01 +0530

    Gathering data for the report(parts - a,b)

--- NaiveBayes/naive.py
+++ NaiveBayes/naive.py
@@ -6,6 +6,7 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 import nltk
+from nltk.stem.porter import PorterStemmer
 
 TRAIN_FILE = "../../col341_a2_data/amazon_train.csv"
 TEST_FILE = "../../col341_a2_data/amazon_test_public.csv"
@@ -13,8 +14,9 @@ RE1 = "\w+"
 STOP = set(stopwords.words('english'))
 with open('ranksnl_large.txt',"r") as fl:
     STOP2=set(fl.read().split())
-STOPLEV2 = False
-SNOW = SnowballStemmer("english")
+STOPLEV2 = True
+# SNOW = SnowballStemmer("english")
+SNOW = PorterStemmer()
 
 def load_data(file_path,clip=False):
 	dat = pd.read_csv(file_path,header=None,index_col=None)	
@@ -164,25 +166,51 @@ test_data = load_data(TEST_FILE)
 # model = train_model(train_n,vocab,inv_vocab)
 # train_n = removeStopwords(train_data)
 # test_n = removeStopwords(test_data)
-
+train_n =filter_data(train_data,True,False)
+test_n = filter_data(test_data,True,False)
+#############################   PART A ######################################
+vocab = create_vocabulary(train_n['reviews'])
+inv_vocab = invert_vocab(vocab)
+model = train_model_freq(train_n,vocab,inv_vocab,n_grams=1)
+predictions = [int(predict_freq(rev,model,vocab,inv_vocab,n_grams=1)) for rev in test_n["reviews"]]
+print (np.bincount(predictions))
+#################################Modified##################################33
 stopstem = [(False,False),(False,True),(True,False),(True,True)]
 gram = [1,2,3]
 stop2 = [True,False]
-for i in range(4):
-	stop = stopstem[i][0]
-	stem = stopstem[i][1]
-	print ("STOP :",stop,"\nSTEM :",stem,"\n---------")
-	for k in range(2):
-		STOPLEV2 = stop2[k]
-		print ("STOP2 : ",STOPLEV2)
-		train_n = filter_data(train_data,stop,stem)
-		test_n = filter_data(test_data,stop,stem)
-		for j in range(3):
-			print ("GRAMS: ",j+1)
-			vocab = create_vocabulary(train_n['reviews'],n_grams=j)
-			inv_vocab = invert_vocab(vocab)
-			model = train_model_freq(train_n,vocab,inv_vocab,n_grams=j)
-			predictions = [int(predict_freq(rev,model,vocab,inv_vocab,n_grams=j)) for rev in test_n["reviews"]]
-			print (np.bincount(predictions))
-			np.savetxt("test_preds_freq/"+str(i)+""+str(k)+""+str(j)+".txt",predictions,fmt="%i")
+# for i in range(4):
+# 	stop = stopstem[i][0]
+# 	stem = stopstem[i][1]
+# 	print ("STOP :",stop,"\nSTEM :",stem,"\n---------")
+# 	for k in range(2):
+# 		STOPLEV2 = stop2[k]
+# 		print ("STOP2 : ",STOPLEV2)
+# 		train_n = filter_data(train_data,stop,stem)
+# 		test_n = filter_data(test_data,stop,stem)
+# 		for j in range(3):
+# 			print ("GRAMS: ",j+1)
+# 			vocab = create_vocabulary(train_n['reviews'],n_grams=j)
+# 			inv_vocab = invert_vocab(vocab)
+# 			model = train_model_freq(train_n,vocab,inv_vocab,n_grams=j)
+# 			predictions = [int(predict_freq(rev,model,vocab,inv_vocab,n_grams=j)) for rev in test_n["reviews"]]
+# 			print (np.bincount(predictions))
+# 			np.savetxt("test_preds_freq/"+str(i)+""+str(k)+""+str(j)+".txt",predictions,fmt="%i")
 # predictions = [int(predict(rev,model,vocab,inv_vocab)) for rev in test_n["reviews"]]
+
+
+# for i in range(4):
+# 	stop = stopstem[i][0]
+# 	stem = stopstem[i][1]
+# 	print ("STOP :",stop,"\nSTEM :",stem,"\n---------")
+# 	for k in range(2):
+# 		STOPLEV2 = stop2[k]
+# 		print ("STOP2 : ",STOPLEV2)
+# 		train_n = filter_data(train_data,stop,stem)
+# 		test_n = filter_data(test_data,stop,stem)
+# 		vocab = create_vocabulary(train_n['reviews'],n_grams=1)
+# 		inv_vocab = invert_vocab(vocab)
+# 		model = train_model_freq(train_n,vocab,inv_vocab,n_grams=1)
+# 		predictions = [int(predict_freq(rev,model,vocab,inv_vocab,n_grams=1)) for rev in test_n["reviews"]]
+# 		print (np.bincount(predictions))
+		# np.savetxt("test_preds_freq/"+str(i)+""+str(k)+""+str(j)+".txt",predictions,fmt="%i")
+
