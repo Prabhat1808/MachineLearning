34c3b746a35320c34d96e0e8a2b11047c35821de
Prabhat1808 <prabhatkanaujia08@gmail.com>
2018-09-07 09:31:18 +0530

    Report creation left

--- NaiveBayes/naive.py
+++ NaiveBayes/naive.py
@@ -7,6 +7,9 @@ from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 import nltk
 from nltk.stem.porter import PorterStemmer
+from scipy.sparse import lil_matrix, csr_matrix, coo_matrix
+import scipy as sp
+from sklearn.preprocessing import normalize
 
 TRAIN_FILE = "../../col341_a2_data/amazon_train.csv"
 TEST_FILE = "../../col341_a2_data/amazon_test_public.csv"
@@ -79,24 +82,6 @@ def train_model(data,vocab,inv_vocab,n_grams=1):
 	labels = data["labels"]
 	cc = np.zeros(len(set(labels)))
 	wc = np.zeros((len(set(labels)),len(vocab)))
-	# wf = {vocab[i]:0 for i in range((len(vocab)))}
-	for i in range(len(labels)):
-		tmp = np.zeros(len(vocab))
-		rev = reviews[i]
-		lab = labels[i]
-		cc[lab-1] += 1
-		words = get_tokens(rev,n_grams=n_grams)
-		for word in words:
-			tmp[inv_vocab[word]] = 1
-			# wf[word] += 1
-		wc[lab-1] += tmp
-	return {"cc":cc,"wc":wc}
-
-def train_model_freq(data,vocab,inv_vocab,n_grams=1):
-	reviews = data["reviews"]
-	labels = data["labels"]
-	cc = np.zeros(len(set(labels)))
-	wc = np.zeros((len(set(labels)),len(vocab)))
 	ls = np.zeros(len(set(labels)))
 	# wf = {vocab[i]:0 for i in range((len(vocab)))}
 	for i in range(len(labels)):
@@ -109,31 +94,40 @@ def train_model_freq(data,vocab,inv_vocab,n_grams=1):
 			ls[lab-1] += 1
 	return {"cc":cc,"wc":wc,"ls":ls}
 
-def predict(review,model,vocab,inv_vocab,n_grams=1):
-	cc = model["cc"]
-	tot = np.sum(cc)
-	wc = model["wc"]
-	# wf = model["wf"]
-	# norm = np.divide(wc,(np.sum(wc,axis=1)).reshape((5,1)))
-	prob = np.zeros(len(cc))
-	words = get_tokens(review,n_grams=n_grams)
-
-	for label in range(len(cc)):
-		tmp = np.log(cc[label]/tot)
-		# tmp = 0
-		den = cc[label] + len(vocab) + 1
-		# den = cc[label] +  1
+def train_model_tfidf(data,vocab,inv_vocab,n_grams=1):
+	reviews = data["reviews"]
+	labels = data["labels"]
+	cc = np.zeros(len(set(labels)))
+	wc = np.zeros((len(set(labels)),len(vocab)))
+	# ls = np.zeros(len(set(labels)))
+	tf = lil_matrix((len(labels),len(vocab)))
+	# wf = {vocab[i]:0 for i in range((len(vocab)))}
+	for i in range(len(labels)):
+		tmp = np.zeros(len(vocab))
+		rev = reviews[i]
+		lab = labels[i]
+		cc[lab-1] += 1
+		words = get_tokens(rev,n_grams=n_grams)
 		for word in words:
-			try:
-				occurence = wc[label][inv_vocab[word]]
-				# occurence = wc[label][inv_vocab[word]]/wf[word]
-			except:
-				occurence = 0
-			tmp += np.log((occurence+1)/den)
-		prob[label] = tmp
-	return np.argmax(prob)+1
+			tmp[inv_vocab[word]] = 1
+			tf[i,inv_vocab[word]] += 1
+			# ls[lab-1] += 1
+		wc[lab-1] += tmp
+	tf = (normalize(tf)).tocsr()
+	wc = np.log(len(labels)/ (np.sum(wc,axis=0) + 1))
+	tf = (tf.multiply(wc)).tocsr()
+	# print (type(tf))
 
-def predict_freq(review,model,vocab,inv_vocab,n_grams=1):
+	# wc_n = [np.zeros(len(vocab)) for _ in range(len(set(labels)))]
+	wc_n = np.zeros((len(set(labels)),len(vocab)))
+	for i in range(len(labels)):
+		wc_n[labels[i]-1] = wc_n[labels[i]-1] + tf[i]
+
+	ls = np.sum(wc_n,axis=1)
+
+	return {"cc":cc,"wc":wc_n,"ls":ls}
+
+def predict(review,model,vocab,inv_vocab,n_grams=1):
 	cc = model["cc"]
 	tot = np.sum(cc)
 	wc = model["wc"]
@@ -169,11 +163,11 @@ test_data = load_data(TEST_FILE)
 train_n =filter_data(train_data,True,False)
 test_n = filter_data(test_data,True,False)
 #############################   PART A ######################################
-vocab = create_vocabulary(train_n['reviews'])
+vocab = create_vocabulary(train_n['reviews'],n_grams=1)
 inv_vocab = invert_vocab(vocab)
-model = train_model_freq(train_n,vocab,inv_vocab,n_grams=1)
-predictions = [int(predict_freq(rev,model,vocab,inv_vocab,n_grams=1)) for rev in test_n["reviews"]]
-print (np.bincount(predictions))
+model = train_model_tfidf(train_n,vocab,inv_vocab,n_grams=1)
+predictions = [int(predict(rev,model,vocab,inv_vocab,n_grams=1)) for rev in train_n["reviews"]]
+# print (np.bincount(predictions))
 #################################Modified##################################33
 stopstem = [(False,False),(False,True),(True,False),(True,True)]
 gram = [1,2,3]
@@ -197,7 +191,7 @@ stop2 = [True,False]
 # 			np.savetxt("test_preds_freq/"+str(i)+""+str(k)+""+str(j)+".txt",predictions,fmt="%i")
 # predictions = [int(predict(rev,model,vocab,inv_vocab)) for rev in test_n["reviews"]]
 
-
+from sklearn.metrics import accuracy_score, f1_score
 # for i in range(4):
 # 	stop = stopstem[i][0]
 # 	stem = stopstem[i][1]
